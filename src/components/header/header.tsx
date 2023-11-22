2fa23ca36c7b0f044645e7085b60ae6aa090ef70
Remych04 <remych04@gmail.com>
2023-11-22 18:25:08 +0100

    add header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,14 @@
 import styles from "./header.module.css";
+import Search from "../../assets/header/search.svg?react";
+import Notification from "../../assets/header/notification.svg?react";
+import avatar from "../../assets/header/avatar.webp";
 
 export function Header() {
-  return <header className={styles.header}>Header</header>;
+  return (
+    <header className={styles.header}>
+      <Search className={styles.icon} width={24} height={24} />
+      <Notification className={styles.icon} width={24} height={24} />
+      <img className={styles.avatar} src={avatar} alt="avatar" />
+    </header>
+  );
 }
